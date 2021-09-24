1299de6346a6527074dc0343867499310e7c557c
= <=>
2021-09-24 18:03:59 +0200

    created menu display

--- TCPServer/src/TCPServer.cpp
+++ TCPServer/src/TCPServer.cpp
@@ -1,1 +1,6 @@
 #include "TCPServer.h"
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
--- XPLMServer/src/main.cpp
+++ XPLMServer/src/main.cpp
@@ -3,12 +3,14 @@
 
 #include <XPLMProcessing.h>
 #include <XPLMUtilities.h>
+#include <XPLMMenus.h>
 
 #include <Dataref.h>
 #include <CallbackManager.h>
 #include <utils.h>
 
 #include <nlohmann/json.hpp>
+#include <CallbackManager/src/utils.cpp>
 
 using json = nlohmann::json;
 
@@ -18,7 +20,9 @@ static Dataref visibility;
 static int counter = 0;
 float InitializerCallback(float elapsedSinceCall, float elapsedSinceLastTime, int inCounter, void* inRef);
 float LoopCallback(float elapsedSinceCall, float elapsedSinceLastTime, int inCounter, void* inRef);
-
+static int g_menu_container_idx; // The index of our menu item in the Plugins menu
+static XPLMMenuID g_menu_id; // The menu container we'll append all our menu items to
+void menu_handler_callback(void*, void*);
 
 
 PLUGIN_API int XPluginStart(char* outName, char* outSig, char* outDesc)
@@ -39,6 +43,16 @@ PLUGIN_API int XPluginStart(char* outName, char* outSig, char* outDesc)
 	strcpy(outName, sig.c_str());
 	strcpy(outSig, "eskystudio.tools.XPLMServer");
 	strcpy(outDesc, description.c_str());
+
+	g_menu_container_idx = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "XPLM Server Debug", 0, 0);
+	g_menu_id = XPLMCreateMenu("XPLM Server Debug", XPLMFindPluginsMenu(), 
+		g_menu_container_idx, menu_handler_callback, NULL);
+	XPLMAppendMenuItem(g_menu_id, "Set CAVOK Visibility", (void*)"1", 1);
+	XPLMAppendMenuItem(g_menu_id, "Set  LVO  Visibility", (void*)"2", 1);
+	XPLMAppendMenuItem(g_menu_id, "Set  000%  Rain", (void*)"3", 1);
+	XPLMAppendMenuItem(g_menu_id, "Set  10%  Rain", (void*)"4", 1);
+	XPLMAppendMenuItem(g_menu_id, "Get Visibility", (void*)"5", 1);
+	XPLMAppendMenuItem(g_menu_id, "Get Rain", (void*)"6", 1);
 	return 1;
 }
 
@@ -53,25 +67,20 @@ PLUGIN_API void XPluginDisable(void)
 PLUGIN_API int  XPluginEnable(void)
 { 
 	XPLMDebugString("[XPLMServer]Enabled");
-	std::string dllPath;
-	#ifdef WIN64
-		#ifdef _DEBUG
-			dllPath = PluginConfiguration["DLLFiles"]["Win64"]["Debug"];
-			//            .\Resources\ plugins\ XPLMServer\ DefaultCallbacks64-d.dll
-			//dllPath = ".\\Resources\\plugins\\XPLMServer\\DefaultCallbacks_64-d.dll";
-		#else
-			dllPath = PluginConfiguration["DLLFiles"]["Win64"]["Release"];
-			//dllPath = ".\\Resources\\plugins\\XPLMServer\\DefaultCallbacks_64.dll";
-		#endif
+	std::string configuration;
+	#ifndef _DEBUG
+		configuration = "Release";
 	#else
-		#ifdef _DEBUG
-			dllPath = PluginConfiguration["DLLFiles"]["Win32"]["Debug"];
-			//dllPath = ".\\Resources\\plugins\\XPLMServer\\DefaultCallbacks-d.dll";
-		#else
-			dllPath = PluginConfiguration["DLLFiles"]["Win32"]["Release"];
-			//dllPath = ".\\Resources\\plugins\\XPLMServer\\DefaultCallbacks.dll";
-		#endif
+		configuration = "Debug";
 	#endif
+	std::string platform;
+	#ifndef WIN64
+	platform = "Win32";
+	#else
+	platform = "Win64";
+	#endif
+	std::string dllPath = PluginConfiguration["DLLFiles"][platform][configuration].get<std::string>();
+	
 	std::string dllLog = "[XPLMServer]Trying to load dll from path : '" + dllPath + "'\n";
 	XPLMDebugString(dllLog.c_str());
 	XPLMDebugString("[XPLMServer]Creating a callback manager...\n");
@@ -104,41 +113,91 @@ float InitializerCallback(float elapsedSinceCall, float elapsedSinceLastTime, in
 	data["Link"] = "sim/weather/visibility_reported_m";
 	int res = callbackManager->ExecuteCallback(&data);
 	XPLMDebugString(("[XPLMServer] CallbackManager::ExecuteCallback returned :'" + std::to_string(res) + "'\n").c_str());
-	XPLMSetFlightLoopCallbackInterval(LoopCallback, 1.0, 1, nullptr);
+	//XPLMSetFlightLoopCallbackInterval(LoopCallback, 1.0, 1, nullptr);
  	return 0.0f;
 }
 
-float LoopCallback(float elapsedSinceCall, float elapsedSinceLastTime, int inCounter, void* inRef)
+//float LoopCallback(float elapsedSinceCall, float elapsedSinceLastTime, int inCounter, void* inRef)
+//{
+//	json setData1;
+//	setData1["Operation"] = "SET_REG_DATA";
+//	setData1["Name"] = "VISIBILITY";
+//	json setData2;
+//	setData2["Operation"] = "SET_DATA";
+//	setData2["Link"] = "sim/weather/rain_percent";
+//	if (counter % 2 == 0)
+//	{
+//		setData1["Value"] = "500.0";
+//		setData2["Value"] = "0.0";
+//	}
+//	else
+//	{
+//		setData1["Value"] = "10000.0";
+//		setData2["Value"] = "1.0";
+//	}
+//	int res = callbackManager->ExecuteCallback(&setData1);
+//	int res2 = callbackManager->ExecuteCallback(&setData2);
+//	counter++;
+//	json getData1;
+//	getData1["Operation"] = "GET_REG_DATA";
+//	getData1["Name"] = "VISIBILITY";
+//	json getData2;
+//	getData2["Operation"] = "GET_DATA";
+//	getData2["Link"] = "sim/weather/rain_percent";
+//	
+//	callbackManager->ExecuteCallback(&getData1);
+//	callbackManager->ExecuteCallback(&getData2);
+//	XPLMSpeakString(("Visibility : " + getData1["Value"].get<std::string>() +
+//		" meters\nRain : " + getData2["Value"].get<std::string>() + " percent").c_str());
+//	return 5.0f;
+//}
+
+
+void menu_handler_callback(void* in_menu_ref, void* in_item_ref)
 {
-	json setData1;
-	setData1["Operation"] = "SET_REG_DATA";
-	setData1["Name"] = "VISIBILITY";
-	json setData2;
-	setData2["Operation"] = "SET_DATA";
-	setData2["Link"] = "sim/weather/rain_percent";
-	if (counter % 2 == 0)
+	json operation;
+	if (!strcmp((const char*)in_item_ref, "1"))
 	{
-		setData1["Value"] = "500.0";
-		setData2["Value"] = "0.0";
+		operation["Operation"] = "SET_REG_DATA";
+		operation["Name"] = "VISIBILITY";
+		operation["Value"] = "10000.0";
 	}
-	else
+	else if (!strcmp((const char*)in_item_ref, "2"))
 	{
-		setData1["Value"] = "1.0";
-		setData2["Value"] = "10000.0";
+		operation["Operation"] = "SET_REG_DATA";
+		operation["Name"] = "VISIBILITY";
+		operation["Value"] = "500.0";
 	}
-	int res = callbackManager->ExecuteCallback(&setData1);
-	int res2 = callbackManager->ExecuteCallback(&setData2);
-	counter++;
-	json getData1;
-	getData1["Operation"] = "GET_REG_DATA";
-	getData1["Name"] = "VISIBILITY";
-	json getData2;
-	getData2["Operation"] = "GET_DATA";
-	getData2["Link"] = "sim/weather/rain_percent";
-	
-	callbackManager->ExecuteCallback(&getData1);
-	callbackManager->ExecuteCallback(&getData2);
-	XPLMSpeakString(("Visibility : " + getData1["Value"].get<std::string>() +
-		" meters\nRain : " + getData2["Value"].get<std::string>() + " percent").c_str());
-	return 5.0f;
+	else if (!strcmp((const char*)in_item_ref, "3"))
+	{
+		operation["Operation"] = "SET_DATA";
+		operation["Link"] = "sim/weather/rain_percent";
+		operation["Value"] = "0.0";
+	}
+	else if (!strcmp((const char*)in_item_ref, "4"))
+	{
+		operation["Operation"] = "SET_DATA";
+		operation["Link"] = "sim/weather/rain_percent";
+		operation["Value"] = "1.0";
+	}
+	else if (!strcmp((const char*)in_item_ref, "5"))
+	{
+		operation["Operation"] = "GET_REG_DATA";
+		operation["Name"] = "VISIBILITY";
+		int res = callbackManager->ExecuteCallback(&operation);
+		callbackManager->Log("Execution returned " + std::to_string(res));
+		XPLMSpeakString(("Visibility : " + operation["Value"].get<std::string>() + " meters").c_str());
+		return;
+	}
+	else if (!strcmp((const char*)in_item_ref, "6"))
+	{
+		operation["Operation"] = "GET_DATA";
+		operation["Link"] = "sim/weather/rain_percent";
+		int res = callbackManager->ExecuteCallback(&operation);
+		callbackManager->Log("Execution returned " + std::to_string(res));
+		XPLMSpeakString(("Rain : " + operation["Value"].get<std::string>() + " percent").c_str());
+		return;
+	}
+	int res = callbackManager->ExecuteCallback(&operation);
+	callbackManager->Log("Execution returned " + std::to_string(res));
 }
